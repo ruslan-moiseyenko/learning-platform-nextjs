ed8234f419272c5ebb0eb49c32936c578d812cd5
Ruslan Moiseyenko <50419466+ruslan-moiseyenko@users.noreply.github.com>
2025-01-04 20:08:13 +0200

    added Toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Sans } from "next/font/google";
 import { Providers } from "@/app/providers";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
         <body className={`${dmSans.className}`}>
           <Providers>
             <div className="root-layout">{children}</div>
+            <Toaster richColors closeButton />
           </Providers>
         </body>
       </html>
